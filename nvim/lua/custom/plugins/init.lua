6b2e1eb1b72d9175965b27c179d552c6177c0a5c
Leaves333 <canchenli@gmail.com>
2024-08-15 09:23:07 -0500

    inital commit for real this time

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,121 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+
+	-- lualine: nicer line at the bottom, complete with fancy icons
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lualine").setup({
+				icons_enabled = true,
+				theme = "ayu-dark",
+			})
+		end,
+	},
+
+	-- {
+	-- 	"epwalsh/obsidian.nvim",
+	-- 	version = "*", -- recommended, use latest release instead of latest commit
+	-- 	lazy = true,
+	-- 	ft = "markdown",
+	-- 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- 	-- event = {
+	-- 	-- 	-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	-- 	-- 	-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+	-- 	-- 	-- refer to `:h file-pattern` for more examples
+	-- 	-- 	"BufReadPre home/leaves/Obsidian/*.md",
+	-- 	-- 	"BufNewFile home/leaves/Obsidian/*.md",
+	-- 	-- },
+	--
+	-- 	dependencies = {
+	-- 		-- Required.
+	-- 		"nvim-lua/plenary.nvim",
+	--
+	-- 		-- see below for full list of optional dependencies 👇
+	-- 	},
+	--
+	-- 	opts = {
+	--
+	-- 		workspaces = {
+	-- 			{
+	-- 				name = "obsidian",
+	-- 				path = "~/Obsidian",
+	-- 			},
+	-- 		},
+	--
+	-- 		-- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+	-- 		completion = {
+	-- 			-- Set to false to disable completion.
+	-- 			nvim_cmp = true,
+	-- 			-- Trigger completion at 2 chars.
+	-- 			min_chars = 2,
+	-- 		},
+	--
+	-- 		daily_notes = {
+	-- 			-- Optional, if you keep daily notes in a separate directory.
+	-- 			folder = "dailies",
+	-- 			-- Optional, if you want to change the date format for the ID of daily notes.
+	-- 			date_format = "%Y-%m-%d",
+	-- 			-- Optional, if you want to change the date format of the default alias of daily notes.
+	-- 			-- alias_format = "%B %-d, %Y",
+	-- 			-- Optional, default tags to add to each new daily note created.
+	-- 			default_tags = { "daily-notes" },
+	-- 			-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+	-- 			template = nil,
+	-- 		},
+	--
+	-- 		-- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+	-- 		-- way then set 'mappings = {}'.
+	-- 		mappings = {
+	-- 			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+	-- 			["gf"] = {
+	-- 				action = function()
+	-- 					return require("obsidian").util.gf_passthrough()
+	-- 				end,
+	-- 				opts = { noremap = false, expr = true, buffer = true },
+	-- 			},
+	-- 			-- Toggle check-boxes.
+	-- 			["<leader>ch"] = {
+	-- 				action = function()
+	-- 					return require("obsidian").util.toggle_checkbox()
+	-- 				end,
+	-- 				opts = { buffer = true },
+	-- 			},
+	-- 			-- Smart action depending on context, either follow link or toggle checkbox.
+	-- 			["<cr>"] = {
+	-- 				action = function()
+	-- 					return require("obsidian").util.smart_action()
+	-- 				end,
+	-- 				opts = { buffer = true, expr = true },
+	-- 			},
+	-- 		},
+	--
+	-- 		picker = {
+	-- 			-- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+	-- 			name = "telescope.nvim",
+	-- 			-- Optional, configure key mappings for the picker. These are the defaults.
+	-- 			-- Not all pickers support all mappings.
+	-- 			note_mappings = {
+	-- 				-- Create a new note from your query.
+	-- 				new = "<C-x>",
+	-- 				-- Insert a link to the selected note.
+	-- 				insert_link = "<C-l>",
+	-- 			},
+	-- 			tag_mappings = {
+	-- 				-- Add tag(s) to current note.
+	-- 				tag_note = "<C-x>",
+	-- 				-- Insert a tag at the current location.
+	-- 				insert_tag = "<C-l>",
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
+
+	-- exciting vimbegood action !!!!
+	"ThePrimeagen/vim-be-good",
+}
