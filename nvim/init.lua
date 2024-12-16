82b6360a674995232d0812d201703631c31a8b0b
Leaves333 <canchenli@gmail.com>
2024-12-15 18:32:28 -0600

    fix tabstop thing

--- nvim/init.lua
+++ nvim/init.lua
@@ -33,7 +33,7 @@ vim.opt.showmode = false
 vim.opt.breakindent = true
 
 -- set tabs to be 4 spaces instead of a tab char
--- vim.o.tabstop = 4
+vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
